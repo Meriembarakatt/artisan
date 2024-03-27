2dbd40e6dfb8c683cdb425b5db8503ac9d297aa5
ElhassIkram <elhassikram45@gmail.com>
2024-03-27 12:58:52 +0100

    ajoute

---
+++ app/Http/Controllers/ReglementVenteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\reglementVente;
+use Illuminate\Http\Request;
+
+class ReglementVenteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\reglementVente  $reglementVente
+     * @return \Illuminate\Http\Response
+     */
+    public function show(reglementVente $reglementVente)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\reglementVente  $reglementVente
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(reglementVente $reglementVente)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\reglementVente  $reglementVente
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, reglementVente $reglementVente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\reglementVente  $reglementVente
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(reglementVente $reglementVente)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_03_27_115101_create_reglement_ventes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reglement_ventes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('artisan_id')->constrained('artisans');
+            $table->foreignId('mode_id')->constrained('modes');
+            $table->date('date');
+            $table->string('montant');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reglement_ventes');
+    }
+};
