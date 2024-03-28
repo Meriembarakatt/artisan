d6915a0739a294aa0524e1e44ebd4bbb865e1387
m.barakat <meriembarakat30@gmail.com>
2024-03-28 12:22:09 +0100

    Committing local changes

--- database/migrations/2024_03_27_233103_create_reglement_artisans_table.php
+++ database/migrations/2024_03_27_233103_create_reglement_artisans_table.php
@@ -4,32 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateReglementArtisansTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('reglement_artisans', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('artisan_id')->constrained('artisans');
-            $table->foreignId('mode_id')->constrained('modes');
+            $table->bigIncrements('id');
+            $table->bigInteger('artisan_id')->unsigned();
+            $table->bigInteger('mode_id')->unsigned();
             $table->date('date');
-            $table->string('montant ');
+            $table->decimal('montant', 10, 2);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('reglement_artisans');
     }
-};
+}
